9b5eefb3c9332e0491858fc037136522e7199ad4
denghuichao <865871137@qq.com>
2017-06-25 14:51:14 +0800

    first commit

---
+++ src/main/java/com/deng/crawler/App.java
@@ -0,0 +1,13 @@
+package com.deng.crawler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
